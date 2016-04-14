13e46409aa9b4c89bdecedb01142690ddfc933b5
Jesse Spangenberger <flamusdiu@gmail.com>
2016-04-14 06:12:41 -0400

    Version bump

--- src/pia/__init__.py
+++ src/pia/__init__.py
@@ -17,7 +17,7 @@
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = "2.5"
+__version__ = "2.6"
 
 from . import utils, run
 from pia.conf import properties
